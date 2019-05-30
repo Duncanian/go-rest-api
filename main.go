3db5416757de3b1b8dc65fcafb1bd4c13a3b1b80
Ian Duncan <ianduncan08@gmail.com>
2019-05-30 20:57:33 +0300

    create the home endpoint

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func homeLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homeLink)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
