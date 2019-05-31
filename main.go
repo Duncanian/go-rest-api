dfb37d9b292afc5dc1c6002b00a9a623cf593ac8
Ian Duncan <ianduncan08@gmail.com>
2019-05-31 15:24:13 +0300

    create endpoint for deleting an event

--- main.go
+++ main.go
@@ -54,11 +54,26 @@ func getAllEvents(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(events)
 }
 
+func deleteEvent(w http.ResponseWriter, r *http.Request) {
+	// Get the ID from the url
+	eventID := mux.Vars(r)["id"]
+
+	// Get the details from an existing event
+	// Use the blank identifier to avoid creating a value that will not be used
+	for i, singleEvent := range events {
+		if singleEvent.ID == eventID {
+			events = append(events[:i], events[i+1:]...)
+			fmt.Fprintf(w, "Event with ID %v has been deleted successfully", eventID)
+		}
+	}
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 	router.HandleFunc("/event", createEvent).Methods("POST")
-	router.HandleFunc("/events/{id}", getOneEvent)
+	router.HandleFunc("/events/{id}", getOneEvent).Methods("GET")
 	router.HandleFunc("/events", getAllEvents)
+	router.HandleFunc("/events/{id}", deleteEvent).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
